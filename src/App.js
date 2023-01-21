3358a4d5039b446860aad6ac95374f9b161df101
ElemaGp <elemagodspower@gmail.com>
2023-01-21 10:31:35 +0100

    feat: mui input set up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      okay
+      <img src="https://media0.giphy.com/media/wSv0sqJI7uoqSBPbNc/giphy.gif?cid=790b76111bb243114349dcfa1792a8bb589e9a948d973027&rid=giphy.gif&ct=g" alt="" />
     </div>
   );
 }
