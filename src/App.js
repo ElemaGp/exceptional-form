14f8cf49c449c976b73ddf786105c69e4703e599
ElemaGp <elemagodspower@gmail.com>
2023-01-21 13:08:42 +0100

    feat: add toast notification

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 
 import { Button } from "@mui/material"
 import FormikControl from './components/formikComponents/FormikControl';
+//toast
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Box } from '@mui/system';
 // import { useSelector, useDispatch } from 'react-redux'
 // import { login } from "../../components/reduxToolkit/features/userSlice"
 // import usePasswordToggle from "../../usePasswordToggle"
@@ -24,16 +28,27 @@ function App() {
    
   })
 
-  const onSubmit = values => {    //alternatively, i can just destructure this "values" object to directly get the email and password.
-    console.log('Form data', values)
-    
-  }
+  const onSubmit = (values) => {    //alternatively, i can just destructure this "values" object to directly get the email and password.
+
+
+    toast.success("You are invited to the party!", {     //you can also directly write the alert you want to display as a string in the bracket.
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+  })
+
+}
  
 
 
   return (
     
-    
+  <Box>
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
@@ -67,13 +82,16 @@ function App() {
 
               
               {/* <button type='submit' disabled={!formik.isValid} className={style.btn}>Sign Up</button> "formik.isValid is false whenever there is an error" */}
-              <img src="https://media0.giphy.com/media/wSv0sqJI7uoqSBPbNc/giphy.gif?cid=790b76111bb243114349dcfa1792a8bb589e9a948d973027&rid=giphy.gif&ct=g" alt="dancing" className='danceBtn'/>
+              <img src="https://media0.giphy.com/media/wSv0sqJI7uoqSBPbNc/giphy.gif?cid=790b76111bb243114349dcfa1792a8bb589e9a948d973027&rid=giphy.gif&ct=g" alt="dancing" className='danceBtn' onClick={onSubmit}/>
             </div>
 
           </Form>
         )
       }}
+      
     </Formik>
+    <ToastContainer /> {/*you can also style this toast container component if you want it to affect all the toast alerts in this component (unless you then style that alert seperately) */}
+  </Box>
     
   )
 }
